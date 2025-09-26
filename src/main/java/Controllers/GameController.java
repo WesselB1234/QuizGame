70408aef4ab56f43547cdedfd9228cb04f79ab53
WesselB1234 <stosin007@hotmail.com>
2025-09-26 21:44:44 +0200

    Game multiple pages

--- src/main/java/Controllers/GameController.java
+++ src/main/java/Controllers/GameController.java
@@ -5,9 +5,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class GameController {
 
+    @FXML
+    private VBox nameSelectorPage;
+    @FXML
+    private VBox questionsPage;
     @FXML
     private Label errorLbl;
     @FXML
@@ -16,13 +21,21 @@ public class GameController {
     private TextField nameTextField;
     private QuizGame quizGame;
 
-    @FXML
-    protected void onGameStart(ActionEvent event) {
-
-    }
-
     public void setQuizGame(QuizGame quizGame) {
         this.quizGame = quizGame;
         quizNameLbl.setText("Quiz: " + quizGame.title);
+    }
+
+    @FXML
+    protected void onGameStart(ActionEvent event) {
+
+        if(nameTextField.getText().isEmpty()) {
+            errorLbl.setText("Please enter your name.");
+            errorLbl.setVisible(true);
+        }
+        else if(quizGame != null){
+            nameSelectorPage.setVisible(false);
+            questionsPage.setVisible(true);
+        }
     }
 }
