bd6ef55dcff4109a15bb09c3565403059151bd22
WesselB1234 <stosin007@hotmail.com>
2025-09-27 19:13:25 +0200

    Placeholder results page

--- src/main/java/AppStartup/QuizApplication.java
+++ src/main/java/AppStartup/QuizApplication.java
@@ -11,7 +11,7 @@ public class QuizApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(QuizApplication.class.getResource("/menu-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 350.0, 220);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Menu");
         stage.setScene(scene);
         stage.show();
